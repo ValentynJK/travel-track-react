95882bf1766ed3f7964ca963ae1e0c8e10fe28e9
Valentyn <valentynejk@gmail.com>
2022-08-06 12:32:07 +0200

    places limit - 6

--- server/modules/places.js
+++ server/modules/places.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 export const getPlaces = async (city, category, options) => {
   try {
-    const url = `https://api.foursquare.com/v3/places/search?${category ? 'categories=' + category + '&' : ''}fields=fsq_id,categories,link,location,name,geocodes&sort=POPULARITY&limit=2&near=${city}`;
+    const url = `https://api.foursquare.com/v3/places/search?${category ? 'categories=' + category + '&' : ''}fields=fsq_id,categories,link,location,name,geocodes&sort=POPULARITY&limit=6&near=${city}`;
     const response = await fetch(url, options);
     const responseJSON = await response.json();
     const places = responseJSON.results;
